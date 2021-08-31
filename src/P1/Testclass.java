70bef5aabba679ca3eaca784e7ec486c3492ccb8
Genius Jasani <jasanigenius@gmail.com>
2021-08-31 22:53:17 +0530

    Changes in branch b testclass code

--- src/P1/Testclass.java
+++ src/P1/Testclass.java
@@ -4,8 +4,9 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Hello world12345");
-		System.out.println("Hi there..!!");
+		System.out.println("Hello world12345678910");
+		System.out.println("Hi there..!!Hi there..!");
+		System.out.println("My name is genius");
 	}
 
 }
