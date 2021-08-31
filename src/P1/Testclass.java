d0ed5739051793fda9d27da9b531b76a9ece5369
Genius Jasani <jasanigenius@gmail.com>
2021-08-31 23:16:08 +0530

    Chnages in branch A code

--- src/P1/Testclass.java
+++ src/P1/Testclass.java
@@ -3,8 +3,8 @@ package P1;
 public class Testclass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println("Hello world12345");
+		System.out.println("Hello world is a java program!");
+		System.out.println("Hello world123456789");
 		System.out.println("Hi there..!!");
 	}
 
